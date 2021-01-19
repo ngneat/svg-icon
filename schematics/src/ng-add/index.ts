44c1aac3d3f8118b1be3cd23effa0fee343e504b
vltansky <vl.tansky@gmail.com>
2021-01-19 13:35:43 +0200

    fix(ng-add): missing return

--- schematics/src/ng-add/index.ts
+++ schematics/src/ng-add/index.ts
@@ -127,6 +127,7 @@ process.on('exit', function() {
   const dir = fs.readdirSync(dirPath);
   if(dir.length < 1){
     console.log(`✅️ Ready. Put your svg icons in src/assets/svg and then 'npm run generate-icons'`);
+    return;
   }
   require('child_process').execSync('npm run generate-icons');
   console.log(`✅️ npm run generate-icons`);
