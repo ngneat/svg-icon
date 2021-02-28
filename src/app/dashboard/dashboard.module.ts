b2541c90f8ba42c1335af1c1f55d476e8ea1d981
NetanelBasal <netanel7799@gmail.com>
2021-02-28 16:17:24 +0200

    feat: 🎸 first class support for lazy load

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -5,10 +5,11 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 import { SvgIconsModule } from '@ngneat/svg-icon';
 import { dashboardIcon } from '@app/svg/dashboard';
+import { groupIcons } from '@app/svg/group';
 
 @NgModule({
   declarations: [DashboardComponent],
-  imports: [CommonModule, DashboardRoutingModule, SvgIconsModule.forChild(dashboardIcon)]
+  imports: [CommonModule, DashboardRoutingModule, SvgIconsModule.forChild([dashboardIcon, ...groupIcons])]
 })
 export class DashboardModule {
 }
